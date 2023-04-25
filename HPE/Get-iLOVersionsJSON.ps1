ba96f1343ddb7a8be474ccdeab894bdba35a1d3e
brandongraves08 <brandongraves08@gmail.com>
2023-04-25 13:00:15 -0500

    Update Get-iLOVersionsJSON.ps1

--- HPE/Get-iLOVersionsJSON.ps1
+++ HPE/Get-iLOVersionsJSON.ps1
@@ -19,6 +19,13 @@ function Get-iLOVersion {
   param($iloIPAddress, $iloUsername, $iloPassword)
 
   try {
+    # Check if IP address is online
+    $ping = Test-Connection -ComputerName $iloIPAddress -Count 1 -Quiet
+    if (!$ping) {
+      Write-Host "Unable to reach $iloIPAddress. Skipping."
+      return $null
+    }
+
     # Establish a connection to iLO
     $iloConnection = Connect-HPEiLO -Server $iloIPAddress -Username $iloUsername -Password $iloPassword -DisableCertificateAuthentication -ErrorAction Stop
 
@@ -33,33 +40,14 @@ function Get-iLOVersion {
   }
 }
 
-# Function to generate the list of IP addresses to check
-function Get-IPRange {
-  param($startIP, $endIP)
-
-  $start = [System.Net.IPAddress]::Parse($startIP).GetAddressBytes()
-  [Array]::Reverse($start)
-  $startInt = [System.BitConverter]::ToUInt32($start, 0)
-
-  $end = [System.Net.IPAddress]::Parse($endIP).GetAddressBytes()
-  [Array]::Reverse($end)
-  $endInt = [System.BitConverter]::ToUInt32($end, 0)
-
-  $ipRange = @()
-  for ($i = $startInt; $i -le $endInt; $i++) {
-    $ipBytes = [System.BitConverter]::GetBytes($i)
-    [Array]::Reverse($ipBytes)
-    $ipRange += ([System.Net.IPAddress]::Parse(($ipBytes -join '.'))).ToString()
-  }
-
-  return $ipRange
-}
-
 # Generate the list of IP addresses to check
 $ipRange = Get-IPRange -startIP $ipRangeStart -endIP $ipRangeEnd
 
 # Initialize the array to store iLO version information
 $iloVersions = @()
+
+# Print iLO version discovery started
+Write-Host "iLO version discovery started."
 
 # Iterate through the IP range, get the iLO versions, and store the information in the array
 foreach ($iloIPAddress in $ipRange) {
@@ -69,6 +57,9 @@ foreach ($iloIPAddress in $ipRange) {
       'address' = $iloIPAddress
       'iLOVersion' = $iloVersion
     }
+
+    # Print iLO version discovered
+    Write-Host "iLO version discovered for $iloIPAddress: $iloVersion"
   }
 }
 
@@ -77,3 +68,6 @@ $iloVersions | ConvertTo-Json | Set-Content -Path $outputJsonFile
 
 # Stop logging output to the log file
 Stop-Transcript
+
+# Print iLO version discovery completed
+Write-Host "iLO version discovery completed."
