79b3a8ab39ec6f9801ebb03123e5578f937b342e
brandongraves08 <brandongraves08@gmail.com>
2023-04-25 11:03:38 -0500

    Update Get-iLOVersionsJSON.ps1
    removed need to convert

--- HPE/Get-iLOVersionsJSON.ps1
+++ HPE/Get-iLOVersionsJSON.ps1
@@ -29,31 +29,14 @@ function Get-iLOVersion {
   }
 }
 
-# Function to convert IP address to a decimal number
-function Convert-IPToDecimal {
-  param($ipAddress)
-
-  $octets = $ipAddress.Split('.')
-  return [UInt32]($octets[0] -shl 24) + ($octets[1] -shl 16) + ($octets[2] -shl 8) + $octets[3]
-}
-
-# Function to convert a decimal number to an IP address
-function Convert-DecimalToIP {
-  param($decimal)
-
-  return ('{0}.{1}.{2}.{3}' -f ($decimal -shr 24), ($decimal -shr 16 -band 0xFF), ($decimal -shr 8 -band 0xFF), ($decimal -band 0xFF))
-}
-
-# Calculate the decimal representation of the IP range
-$ipStartDecimal = Convert-IPToDecimal -ipAddress $ipRangeStart
-$ipEndDecimal = Convert-IPToDecimal -ipAddress $ipRangeEnd
+# Generate the list of IP addresses to check
+$ipRange = [System.Net.IPAddress]::Parse($ipRangeStart)..[System.Net.IPAddress]::Parse($ipRangeEnd) | ForEach-Object { $_.GetAddressBytes() -join '.' }
 
 # Initialize the array to store iLO version information
 $iloVersions = @()
 
 # Iterate through the IP range, get the iLO versions, and store the information in the array
-for ($ipDecimal = $ipStartDecimal; $ipDecimal -le $ipEndDecimal; $ipDecimal++) {
-  $iloIPAddress = Convert-DecimalToIP -decimal $ipDecimal
+foreach ($iloIPAddress in $ipRange) {
   $iloVersion = Get-iLOVersion -iloIPAddress $iloIPAddress -iloUsername $iloUsername -iloPassword $iloPasswordPlainText
   if ($iloVersion -ne $null) {
     $iloVersions += @{
